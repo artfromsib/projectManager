7354fe5f05c4d9f34d0c78fe918b63ed22e76d49
artfromsib <artfromsib@gmail.com>
2022-09-05 18:08:51 +0700

    refactoring

--- src/main/java/com/ym/projectManager/service/TrackerService.java
+++ src/main/java/com/ym/projectManager/service/TrackerService.java
@@ -1,33 +1,21 @@
 package com.ym.projectManager.service;
 
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.ym.projectManager.model.Parcel;
-import com.ym.projectManager.model.TrackParcel;
-import com.ym.projectManager.model.comparator.TrackParcelComparator;
 import com.ym.projectManager.repository.ParcelRepository;
 import com.ym.projectManager.repository.TrackParcelRepository;
 import org.springframework.stereotype.Service;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
 public class TrackerService {
 
-    /**
-     * Apikey
-     */
     private String Apikey = "D377580BE09FDBCD101AF34889F3E093";
 
     private final ParcelRepository parcelRepository;
@@ -104,108 +92,9 @@ public class TrackerService {
 
         }
 
-
         return result;
 
-
     }
-
-    public void updateTrackingParcel() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        StringBuilder requestData = new StringBuilder("[");
-        Optional<Parcel> notDeliveredParcel = parcelRepository.findFirstByDeliveredIsFalseAndTrackNumberIsNotNullOrderByLastUpdateDesc();
-
-        if (notDeliveredParcel.isPresent()
-                && (!dateFormat.format(notDeliveredParcel.get().getLastUpdate()).equals(dateFormat.format(new Date())))) {
-
-            Set<Parcel> parcels = parcelRepository.findAllByDeliveredIsFalse();
-            if (parcels != null) {
-                parcels.stream().forEach(parcel -> {
-                    requestData.append("{\"number\": \"" +
-                            parcel.getTrackNumber() + "\"},");
-                });
-
-                requestData.deleteCharAt(requestData.length() - 1).append("]");
-                try {
-                    String result = new TrackerService(parcelRepository, trackParcelRepository).orderOnlineByJson(requestData.toString(), "getTrackInfo");
-                    JsonObject trackInfo = JsonParser.parseString(result).getAsJsonObject();
-                    parseJsonAndSaveData(trackInfo);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-        }
-
-    }
-
-    private void parseJsonAndSaveData(JsonObject trackInfo) {
-        String data0 = trackInfo.get("code").toString();
-
-        if (data0.equals("0")) {
-            JsonObject data = trackInfo.get("data").getAsJsonObject();
-            JsonArray parcels = data.get("accepted").getAsJsonArray();
-
-            for (JsonElement parcel : parcels) {
-                String trackNum = parcel.getAsJsonObject().get("number").getAsString();
-                JsonObject track = parcel.getAsJsonObject().get("track").getAsJsonObject();
-                JsonObject lastStatJson = track.get("z0").getAsJsonObject();
-                Boolean delivered = track.get("e").toString().equals("40");
-
-                StringBuilder lastStatus = new StringBuilder(lastStatJson.get("a").getAsString()).append(", ");
-                lastStatus.append(lastStatJson.get("c").getAsString()).append(", ").append(lastStatJson.get("d").getAsString());
-                lastStatus.append(", ").append(lastStatJson.get("z").getAsString());
-
-
-                JsonArray tracking = track.get("z1").getAsJsonArray();
-                Parcel newParcel = new Parcel(trackNum, lastStatus.toString(), new Date(), delivered);
-                Set<TrackParcel> trackParcel = new HashSet<>();
-
-                for (JsonElement trackStatus : tracking) {
-                    JsonObject statusJson = trackStatus.getAsJsonObject();
-                    String updDate = statusJson.get("a").getAsString();
-
-                    StringBuilder stat = new StringBuilder(statusJson.get("c").getAsString()).append(", ");
-                    stat.append(statusJson.get("d").getAsString()).append(", ").append(statusJson.get("z").getAsString());
-
-                    try {
-                        trackParcel.add(new TrackParcel(stat.toString(), formatter.parse(updDate)));
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(stat);
-                }
-                newParcel.setTrackParcels(trackParcel);
-                saveParcel(newParcel);
-            }
-
-        }
-
-    }
-
-    private void saveParcel(Parcel parcel) {
-        parcel.setParcelId(parcelRepository.findFirstByTrackNumber(parcel.getTrackNumber()).getParcelId());
-        Set<TrackParcel> oldTrackParcel = trackParcelRepository.findAllByParcelOrderByTrackParcelIdDesc(parcel.getParcelId());
-        Set<TrackParcel> newTrackParcel = parcel.getTrackParcels();
-        if (oldTrackParcel != null) {
-            newTrackParcel.removeAll(oldTrackParcel);
-        }
-        if (newTrackParcel.size() > 0) {
-            newTrackParcel.stream().sorted(new TrackParcelComparator().reversed()).forEach(status -> {
-                status.setParcel(parcel);
-                status.setTrackParcelId(trackParcelRepository.save(status).getTrackParcelId());
-            });
-
-            parcel.setTrackParcels(trackParcelRepository.findAllByParcelOrderByTrackParcelIdDesc(parcel.getParcelId()));
-            parcelRepository.saveAndFlush(parcel);
-        } else {
-            parcel.setTrackParcels(oldTrackParcel);
-            parcelRepository.saveAndFlush(parcel);
-        }
-
-    }
-
 
     private String sendPost(String url, Map<String, String> headerParams, List<String> bodyParams, String mothod) {
         OutputStreamWriter out = null;
